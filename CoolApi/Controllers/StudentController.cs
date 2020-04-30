e92006460f7ef06100399df8dbb7da95d326e26b
chris watia <cwatia@seaburysolutions.com>
2020-04-30 15:47:20 +0300

    Added controller and implented GET method

---
+++ CoolApi/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using CoolApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoolApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        
+        //Create student context
+        private StudentContext _studentContext;
+
+        public StudentController(StudentContext context)
+        {
+            _studentContext = context;
+        }
+
+         // GET api/values
+         [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            return _studentContext.Students.ToList();
+        }
+
+        ~StudentController()
+        {
+            _studentContext.Dispose();
+        }
+    }
+}
